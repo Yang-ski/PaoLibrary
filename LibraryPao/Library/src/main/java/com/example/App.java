a94dc9c7537baa781e07d34676a5abe04b666c40
Yangski <lolzgaming911tryhardninja@gmail.com>
2026-01-22 23:57:14 +0800

    changed ui

--- LibraryPao/Library/src/main/java/com/example/App.java
+++ LibraryPao/Library/src/main/java/com/example/App.java
@@ -24,7 +24,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.initStyle(StageStyle.DECORATED);
-        scene = new Scene(loadFXML("fxml/initialNavBar"), 1200, 800);
+        scene = new Scene(loadFXML("fxml/contentContainer"), 1200, 800);
         scene.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.ESCAPE) {
                 stage.setFullScreen(!stage.isFullScreen());
---
+++ LibraryPao/Library/src/main/java/com/example/contentContainerController.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+public class contentContainerController {
+
+    @FXML
+    private Button aboutButton;
+
+    @FXML
+    private Button branchesButton;
+
+    @FXML
+    private StackPane contentArea;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private BorderPane imageHeader;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Text tabName;
+
+}
